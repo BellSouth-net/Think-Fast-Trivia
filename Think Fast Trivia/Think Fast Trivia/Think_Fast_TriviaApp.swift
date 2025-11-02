e059b591983a521b6419ffcdd93749fed09d0cdb
guymorganb <guymorganb@gmail.com>
2025-11-02 11:37:31 -0700

    added blank project

---
+++ Think Fast Trivia/Think Fast Trivia/Think_Fast_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Think_Fast_TriviaApp.swift
+//  Think Fast Trivia
+//
+//  Created by Guy Morgan Beals on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Think_Fast_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
